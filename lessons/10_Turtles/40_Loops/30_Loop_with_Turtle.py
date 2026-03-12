fbdfcbbe221004728063e0c6a86748ba1536303c
Joel-Bracamontes <joelbg1000@gmail.com>
2026-03-12 02:17:31 +0000

    Mision Imposible

--- lessons/10_Turtles/40_Loops/30_Loop_with_Turtle.py
+++ lessons/10_Turtles/40_Loops/30_Loop_with_Turtle.py
@@ -10,7 +10,31 @@ turtle.setup(600,600,0,0)               # Set the size of the window
 tina = turtle.Turtle()                  # Create a turtle named tina
 tina.shape('turtle')                    # Set the shape of the turtle to a turtle                        # Make the turtle move as fast, but not too fast.
 
-tina.goto(0,100)
+tina.goto(0,125)
+tina.begin_fill()
+tina.fillcolor("red")
+for i in range(10):
+    tina.forward(50)
+    tina.left(36)
+tina.end_fill()
+
+tina.goto(0,25)
+tina.begin_fill()
+tina.fillcolor("blue")
+for i in range(10):
+    tina.forward(50)
+    tina.left(36)
+tina.end_fill()
+
+tina.goto(0,-75)
+tina.begin_fill()
+tina.fillcolor("yellow")
+for i in range(10):
+    tina.forward(50)
+    tina.left(36)
+tina.end_fill()
+
+tina.goto(0,-175)
 tina.begin_fill()
 tina.fillcolor("green")
 for i in range(10):
@@ -18,5 +42,6 @@ for i in range(10):
     tina.left(36)
 tina.end_fill()
 
+tina.goto(0,-275)
 
 turtle.exitonclick()
--- lessons/10_Turtles/50_Variables_and_Functions/30_Efficient_Turtle.py
+++ lessons/10_Turtles/50_Variables_and_Functions/30_Efficient_Turtle.py
@@ -6,7 +6,8 @@ Use what you've learned about functions and variables to make a program that
 can draw a square, pentagon, and hexagon with a single function
 """
 
-import turtle                           # Tell Python we want to work with the turtle
+import turtle
+with the turtle                        # Tell Python we want to work with the turtle
 turtle.setup(600,600,0,0)               # Set the size of the window
 
 tina = turtle.Turtle()                  # Create a turtle named tina
@@ -17,6 +18,11 @@ tina.speed(2)                           # Make the turtle move as fast, but not 
 sides=4
 angle=360/sides
 def draw_polygon('sides')
+
+
+    
+
+
 
 
 turtle.exitonclick()                     # Close the window when we click on it
