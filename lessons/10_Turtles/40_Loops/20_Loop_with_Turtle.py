bb526f6d11ae94600b649a215f9e0ee11996ba78
Joel-Bracamontes <joelbg1000@gmail.com>
2026-03-19 02:16:13 +0000

    Suspendido

--- lessons/10_Turtles/40_Loops/20_Loop_with_Turtle.py
+++ lessons/10_Turtles/40_Loops/20_Loop_with_Turtle.py
@@ -8,24 +8,13 @@ but you know you can use a loop to make the program simpler.
 import turtle                           # Tell Python we want to work with the turtle
 turtle.setup(600,600,0,0)               # Set the size of the window
 
-tina = turtle.Turtle()                  # Create a turtle named tina
+t = turtle.Turtle()                  # Create a turtle named tina
 
-tina.shape('turtle')                    # Set the shape of the turtle to a turtle
-tina.speed(2)                           # Make the turtle move as fast, but not too fast.
-
-tina.begin_fill()
-for i in range(4):
-    tina.fillcolor("red")
-    tina.forward(150)
-    tina.left(90)
-tina.end_fill()
-
-tina.goto(-50,50)
-for i in range(3):
-    tina.fillcolor("red")
-    tina.forward(150)
-    tina.left(90)
-tina.end_fill()
+t.goto(50, 0)
+t.goto(65,50)
+t.goto(25,100)
+t.goto(-15,50)
+t.goto(0,0)
     
 
 # Move tina forward by the forward distance
